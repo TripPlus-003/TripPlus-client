3d7927ee141c47a46010f398ca7d3d7e4e39e48b
Madao <gn00678465@gmail.com>
2023-05-15 13:13:57 +0800

    feat: 調整 user account 取得資料方式

--- src/pages/user/account.tsx
+++ src/pages/user/account.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import Image, { type StaticImageData } from 'next/image';
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import { Layout } from '@/components';
 import UserHeader from '@/components/User/user-header';
 import ModalBox, { type ModalState } from '@/components/Modal';
 import type { ReactElement } from 'react';
-import { useState } from 'react';
-import useSWR, { useSWRConfig } from 'swr';
+import { useState, useEffect } from 'react';
+import useSWR, { useSWRConfig, unstable_serialize, SWRConfig } from 'swr';
 import useSWRMutation from 'swr/mutation';
 import {
   apiGetUserAccount,
@@ -31,7 +31,8 @@ import NoImage from '@/assets/images/user/no-image.png';
 import dayjs from 'dayjs';
 import toObject from 'dayjs/plugin/toObject';
 import utc from 'dayjs/plugin/utc';
-import { useSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../../pages/api/auth/[...nextauth]';
 
 dayjs.extend(toObject);
 dayjs.extend(utc);
@@ -47,9 +48,15 @@ const gender = [
   { id: 1, name: '女性' }
 ];
 
-const Account: App.NextPageWithLayout = () => {
+interface Props {
+  token: string;
+  fallback: {
+    [x: string]: Service.RequestResult<ApiAuth.Account>;
+  };
+}
+
+const Account: App.NextPageWithLayout<Props> = ({ token, fallback }) => {
   const { mutate } = useSWRConfig();
-  const { data: session } = useSession();
 
   const [image, setImage] = useState<string | StaticImageData>(NoImage);
   const [file, setFile] = useState<undefined | File>();
@@ -90,32 +97,13 @@ const Account: App.NextPageWithLayout = () => {
   // queries
   const { data: account } = useSWR(
     ['get', '/api/user/account'],
-    () =>
+    async () =>
       apiGetUserAccount({
-        headers: { Authorization: `Bearer ${session?.user.token}` }
+        headers: { Authorization: `Bearer ${token}` }
       }),
     {
-      onSuccess(data, key, config) {
-        if (data && data.status === 'Success') {
-          const { years, months, date } = dayjs(data.data.birthday)
-            .utc(true)
-            .toObject();
-          reset({
-            email: data.data.email,
-            name: data.data.name,
-            nickName: data.data?.nickName ?? undefined,
-            phone: data.data?.phone ?? undefined,
-            gender: data.data?.gender ?? 0,
-            address: data.data?.address ?? undefined,
-            photo: data.data?.photo ?? undefined,
-            country: data.data?.country ?? undefined,
-            introduction: data.data?.introduction ?? undefined,
-            year: isNaN(years) ? undefined : years,
-            month: isNaN(months) ? undefined : months + 1,
-            day: isNaN(date) ? undefined : date
-          });
-          data.data?.photo && setImage(data.data?.photo);
-        }
+      onError(err, key, config) {
+        console.log(err);
       }
     }
   );
@@ -180,224 +168,261 @@ const Account: App.NextPageWithLayout = () => {
     setLoading(false);
   };
 
+  useEffect(() => {
+    if (account && account.status === 'Success') {
+      const { years, months, date } = dayjs(account.data.birthday)
+        .utc(true)
+        .toObject();
+      reset({
+        email: account.data.email,
+        name: account.data.name,
+        nickName: account.data?.nickName ?? undefined,
+        phone: account.data?.phone ?? undefined,
+        gender: account.data?.gender ?? 0,
+        address: account.data?.address ?? undefined,
+        photo: account.data?.photo ?? undefined,
+        country: account.data?.country ?? undefined,
+        introduction: account.data?.introduction ?? undefined,
+        year: isNaN(years) ? undefined : years,
+        month: isNaN(months) ? undefined : months + 1,
+        day: isNaN(date) ? undefined : date
+      });
+      account.data?.photo && setImage(account.data?.photo);
+    }
+  }, [account, reset]);
+
   return (
     <>
       <Head>
         <title>會員中心-個人資料-TripPlus+</title>
       </Head>
 
-      <UserHeader breadcrumb={breadcrumb} />
+      <SWRConfig value={{ fallback }}>
+        <UserHeader breadcrumb={breadcrumb} />
 
-      <Box pt={3} className="pb-10 md:pb-20">
-        <Container maxW={'container.xl'}>
-          <Box backgroundColor={'white'} className="p-5 md:p-10">
-            <Box
-              as="form"
-              onSubmit={handleSubmit(onSubmit)}
-              className="justify-center md:flex"
-            >
-              <Box className="mx-auto mt-10 w-60 md:mx-0 md:mr-28">
-                <Box
-                  borderRadius={'full'}
-                  overflow={'hidden'}
-                  className="relative mx-auto h-60 w-60"
-                >
-                  <Image
-                    src={dataURL || image}
-                    alt="使用者圖片"
-                    width={500}
-                    height={500}
-                    priority
-                  />
+        <Box pt={3} className="pb-10 md:pb-20">
+          <Container maxW={'container.xl'}>
+            <Box backgroundColor={'white'} className="p-5 md:p-10">
+              <Box
+                as="form"
+                onSubmit={handleSubmit(onSubmit)}
+                className="justify-center md:flex"
+              >
+                <Box className="mx-auto mt-10 w-60 md:mx-0 md:mr-28">
+                  <Box
+                    borderRadius={'full'}
+                    overflow={'hidden'}
+                    className="relative mx-auto h-60 w-60"
+                  >
+                    <Image
+                      src={dataURL || image}
+                      alt="使用者圖片"
+                      width={500}
+                      height={500}
+                      priority
+                    />
+                  </Box>
+
+                  <Box mt={6}>
+                    <FormControl
+                      className="flex items-center justify-center rounded py-2"
+                      textAlign={'center'}
+                    >
+                      <label
+                        htmlFor="photo"
+                        className="mr-2 cursor-pointer rounded bg-primary px-3 py-1 text-center text-sm text-white hover:bg-primary-600"
+                      >
+                        選擇檔案
+                      </label>
+                      <Input
+                        id="photo"
+                        type="file"
+                        display={'none'}
+                        accept="image/*"
+                        {...register('photo')}
+                        onChange={onFileChange}
+                      />
+
+                      <span className="truncate text-sm">
+                        {file ? file.name : '未選擇任何檔案'}
+                      </span>
+                    </FormControl>
+                  </Box>
                 </Box>
 
-                <Box mt={6}>
-                  <FormControl
-                    className="flex items-center justify-center rounded py-2"
-                    textAlign={'center'}
-                  >
-                    <label
-                      htmlFor="photo"
-                      className="mr-2 cursor-pointer rounded bg-primary px-3 py-1 text-center text-sm text-white hover:bg-primary-600"
-                    >
-                      選擇檔案
-                    </label>
+                <Box className="w-full md:w-2/5">
+                  <FormControl my={10}>
+                    <FormLabel>暱稱</FormLabel>
                     <Input
-                      id="photo"
-                      type="file"
-                      display={'none'}
-                      accept="image/*"
-                      {...register('photo')}
-                      onChange={onFileChange}
+                      type="text"
+                      {...register('nickName')}
+                      placeholder="請輸入暱稱"
+                    />
+                  </FormControl>
+
+                  <FormControl isRequired my={10}>
+                    <FormLabel>真實身份 / 名稱</FormLabel>
+                    <Input
+                      type="text"
+                      {...register('name', {
+                        required: '請填入真實姓名'
+                      })}
+                      placeholder="請輸入真實身份 / 名稱"
+                    />
+                  </FormControl>
+
+                  <FormControl isInvalid={!!errors.email} my={10}>
+                    <FormLabel className="!font-normal">E-mail</FormLabel>
+                    <Input
+                      type="email"
+                      {...register('email', {
+                        required: '請填入 E-mail',
+                        pattern: {
+                          value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                          message: 'E-mail 格式錯誤'
+                        }
+                      })}
+                      placeholder="請輸入 E-mail"
+                      disabled
                     />
 
-                    <span className="truncate text-sm">
-                      {file ? file.name : '未選擇任何檔案'}
-                    </span>
+                    {!!errors.email && (
+                      <FormErrorMessage className="visible">
+                        {errors.email.message}
+                      </FormErrorMessage>
+                    )}
                   </FormControl>
+
+                  <FormControl isInvalid={!!errors.phone} my={10}>
+                    <FormLabel>手機</FormLabel>
+                    <Input
+                      type="text"
+                      {...register('phone', {
+                        pattern: {
+                          value: /^09\d{8}$/,
+                          message: '手機格式錯誤'
+                        }
+                      })}
+                      placeholder="請輸入手機"
+                    />
+
+                    {!!errors.phone && (
+                      <FormErrorMessage className="visible">
+                        {errors.phone.message}
+                      </FormErrorMessage>
+                    )}
+                  </FormControl>
+
+                  <FormControl my={10}>
+                    <FormLabel>地址</FormLabel>
+                    <Input
+                      type="text"
+                      {...register('address')}
+                      placeholder="請輸入地址"
+                    />
+                  </FormControl>
+
+                  <FormControl my={10}>
+                    <FormLabel>性別</FormLabel>
+                    <Select {...register('gender')}>
+                      {gender.map((item) => (
+                        <option value={item.id} key={item.id}>
+                          {item.name}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+
+                  <FormControl my={10}>
+                    <FormLabel>生日</FormLabel>
+                    <Box className="items-center space-y-3 md:flex md:space-y-0">
+                      <Select placeholder="年" {...register('year')}>
+                        {years.map((year) => (
+                          <option key={year} value={year}>
+                            {year}
+                          </option>
+                        ))}
+                      </Select>
+                      <span className="mx-3 hidden md:block">-</span>
+
+                      <Select placeholder="月" {...register('month')}>
+                        {months.map((month) => (
+                          <option key={month} value={month}>
+                            {month}
+                          </option>
+                        ))}
+                      </Select>
+                      <span className="mx-3 hidden md:block">-</span>
+
+                      <Select placeholder="日" {...register('day')}>
+                        {days.map((day) => (
+                          <option key={day} value={day}>
+                            {day}
+                          </option>
+                        ))}
+                      </Select>
+                    </Box>
+                  </FormControl>
+
+                  <FormControl my={10}>
+                    <FormLabel>來自</FormLabel>
+                    <Input
+                      type="text"
+                      {...register('country')}
+                      placeholder="台北, 台灣"
+                    />
+                  </FormControl>
+
+                  <FormControl my={10}>
+                    <FormLabel>個人介紹</FormLabel>
+                    <Textarea
+                      {...register('introduction')}
+                      placeholder="請輸入個人介紹"
+                    />
+                  </FormControl>
+
+                  <Button
+                    type="submit"
+                    colorScheme="primary"
+                    width={'100%'}
+                    my={4}
+                    isLoading={loading}
+                  >
+                    更新
+                  </Button>
                 </Box>
               </Box>
-
-              <Box className="w-full md:w-2/5">
-                <FormControl my={10}>
-                  <FormLabel>暱稱</FormLabel>
-                  <Input
-                    type="text"
-                    {...register('nickName')}
-                    placeholder="請輸入暱稱"
-                  />
-                </FormControl>
-
-                <FormControl isRequired my={10}>
-                  <FormLabel>真實身份 / 名稱</FormLabel>
-                  <Input
-                    type="text"
-                    {...register('name', {
-                      required: '請填入真實姓名'
-                    })}
-                    placeholder="請輸入真實身份 / 名稱"
-                  />
-                </FormControl>
-
-                <FormControl isInvalid={!!errors.email} my={10}>
-                  <FormLabel className="!font-normal">E-mail</FormLabel>
-                  <Input
-                    type="email"
-                    {...register('email', {
-                      required: '請填入 E-mail',
-                      pattern: {
-                        value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-                        message: 'E-mail 格式錯誤'
-                      }
-                    })}
-                    placeholder="請輸入 E-mail"
-                    disabled
-                  />
-
-                  {!!errors.email && (
-                    <FormErrorMessage className="visible">
-                      {errors.email.message}
-                    </FormErrorMessage>
-                  )}
-                </FormControl>
-
-                <FormControl isInvalid={!!errors.phone} my={10}>
-                  <FormLabel>手機</FormLabel>
-                  <Input
-                    type="text"
-                    {...register('phone', {
-                      pattern: {
-                        value: /^09\d{8}$/,
-                        message: '手機格式錯誤'
-                      }
-                    })}
-                    placeholder="請輸入手機"
-                  />
-
-                  {!!errors.phone && (
-                    <FormErrorMessage className="visible">
-                      {errors.phone.message}
-                    </FormErrorMessage>
-                  )}
-                </FormControl>
-
-                <FormControl my={10}>
-                  <FormLabel>地址</FormLabel>
-                  <Input
-                    type="text"
-                    {...register('address')}
-                    placeholder="請輸入地址"
-                  />
-                </FormControl>
-
-                <FormControl my={10}>
-                  <FormLabel>性別</FormLabel>
-                  <Select {...register('gender')}>
-                    {gender.map((item) => (
-                      <option value={item.id} key={item.id}>
-                        {item.name}
-                      </option>
-                    ))}
-                  </Select>
-                </FormControl>
-
-                <FormControl my={10}>
-                  <FormLabel>生日</FormLabel>
-                  <Box className="items-center space-y-3 md:flex md:space-y-0">
-                    <Select placeholder="年" {...register('year')}>
-                      {years.map((year) => (
-                        <option key={year} value={year}>
-                          {year}
-                        </option>
-                      ))}
-                    </Select>
-                    <span className="mx-3 hidden md:block">-</span>
-
-                    <Select placeholder="月" {...register('month')}>
-                      {months.map((month) => (
-                        <option key={month} value={month}>
-                          {month}
-                        </option>
-                      ))}
-                    </Select>
-                    <span className="mx-3 hidden md:block">-</span>
-
-                    <Select placeholder="日" {...register('day')}>
-                      {days.map((day) => (
-                        <option key={day} value={day}>
-                          {day}
-                        </option>
-                      ))}
-                    </Select>
-                  </Box>
-                </FormControl>
-
-                <FormControl my={10}>
-                  <FormLabel>來自</FormLabel>
-                  <Input
-                    type="text"
-                    {...register('country')}
-                    placeholder="台北, 台灣"
-                  />
-                </FormControl>
-
-                <FormControl my={10}>
-                  <FormLabel>個人介紹</FormLabel>
-                  <Textarea
-                    {...register('introduction')}
-                    placeholder="請輸入個人介紹"
-                  />
-                </FormControl>
-
-                <Button
-                  type="submit"
-                  colorScheme="primary"
-                  width={'100%'}
-                  my={4}
-                  isLoading={loading}
-                >
-                  更新
-                </Button>
-              </Box>
             </Box>
-          </Box>
-        </Container>
-      </Box>
+          </Container>
+        </Box>
 
-      <ModalBox
-        content={modal.content}
-        isOpen={modal.isOpen}
-        onClose={() => setOpenModal(false)}
-        header="提醒"
-        footer={modal.footer}
-      ></ModalBox>
+        <ModalBox
+          content={modal.content}
+          isOpen={modal.isOpen}
+          onClose={() => setOpenModal(false)}
+          header="提醒"
+          footer={modal.footer}
+        ></ModalBox>
+      </SWRConfig>
     </>
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  const res = await apiGetUserAccount({
+    headers: {
+      Authorization: 'Bearer ' + session?.user.token
+    }
+  });
   return {
-    props: {}
+    props: {
+      token: session?.user.token,
+      fallback: {
+        [unstable_serialize(['get', '/api/user/account'])]: res
+      }
+    }
   };
 };
 
